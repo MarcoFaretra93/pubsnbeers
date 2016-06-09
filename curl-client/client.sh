3752004d492fe446ab3f32d197d40fbb31bf4f7d
gabmarini <gab.marini2@gmail.com>
2016-06-09 19:30:07 +0200

    client refactor

--- curl-client/client.sh
+++ curl-client/client.sh
@@ -1,55 +1,61 @@
 #!/usr/bin/env bash
 
 RESULT=`pwd`/curlResult
-echo -e `date` >> $RESULT
 
-#Creazione di tre birre POST request /api/beers
-echo "Creazione di 3 birre, di cui l'ultima è ripetuta" >> $RESULT
-echo '3 POST /api/beers response:' >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer1&type=type1&alcoholic_degree=1" >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" >> $RESULT #creazione fallita, birra già esistente
-echo -e '\n \n' >> $RESULT
+function makeRequests {
 
-#Ricezione di tutte le birre GET request /api/beers
-echo 'Ricezione di tutte le birre' >> $RESULT
-echo '1 GET /api/beers response:' >> $RESULT
-curl http://10.11.1.5/api/beers >> $RESULT
-echo -e '\n \n' >> $RESULT
+    echo -e `date`
 
-#Salvataggio degli id delle birre per eseguire operazioni di ricezione, modifica e rimozione
-OUT=$(curl http://10.11.1.5/api/beers | sed 's/}/}\n/g' | grep id | sed 's/[,]//g' | awk -F \" '{print $4}')
-ar=($OUT)
+    #Creazione di tre birre POST request /api/beers
+    echo "Creazione di 3 birre, di cui l'ultima è ripetuta"
+    echo '3 POST /api/beers response:'
+    curl http://10.11.1.5/api/beers --data "name=beer1&type=type1&alcoholic_degree=1"
+    curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2"
+    curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" #creazione fallita, birra già esistente
+    echo -e '\n \n'
 
-#Ricezione di una birra, GET request /api/beers/$id
-echo 'Ricezione di una birra' >> $RESULT
-echo '1 GET /api/beers/'`echo ${ar[0]}`' response' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Ricezione di tutte le birre GET request /api/beers
+    echo 'Ricezione di tutte le birre'
+    echo '1 GET /api/beers response:'
+    curl http://10.11.1.5/api/beers
+    echo -e '\n \n'
 
-#Modifica di una birra, PUT request /api/beers/$id
-echo 'Modifica di una birra' >> $RESULT
-echo '1 PUT /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Salvataggio degli id delle birre per eseguire operazioni di ricezione, modifica e rimozione
+    OUT=$(curl http://10.11.1.5/api/beers | sed 's/}/}\n/g' | grep id | sed 's/[,]//g' | awk -F \" '{print $4}')
+    ar=($OUT)
 
-#Modifica fallita di una birra, PUT request /api/beers/$id
-echo 'Modifica di una birra' >> $RESULT
-echo '1 PUT /api/beers/000 response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Ricezione di una birra, GET request /api/beers/$id
+    echo 'Ricezione di una birra'
+    echo '1 GET /api/beers/'`echo ${ar[0]}`' response'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}`
+    echo -e '\n \n'
 
-#Rimozione di una birra, DELETE request /api/beers/$id
-echo 'Cancellazione di una birra' >> $RESULT
-echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Modifica di una birra, PUT request /api/beers/$id
+    echo 'Modifica di una birra'
+    echo '1 PUT /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT"
+    echo -e '\n \n'
 
-#Rimozione fallita di una birra, DELETE request /api/beers/$id
-echo 'Cancellazione di una birra non esistente' >> $RESULT
-echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Modifica fallita di una birra, PUT request /api/beers/$id
+    echo 'Modifica di una birra'
+    echo '1 PUT /api/beers/000 response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT"
+    echo -e '\n \n'
 
-#Pulizia dell'ultima birra rimasta
-curl http://10.11.1.5/api/beers/`echo ${ar[1]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" 1> /dev/null 2> /dev/null
+    #Rimozione di una birra, DELETE request /api/beers/$id
+    echo 'Cancellazione di una birra'
+    echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE"
+    echo -e '\n \n'
+
+    #Rimozione fallita di una birra, DELETE request /api/beers/$id
+    echo 'Cancellazione di una birra non esistente'
+    echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE"
+    echo -e '\n \n'
+
+    #Pulizia dell'ultima birra rimasta
+    curl http://10.11.1.5/api/beers/`echo ${ar[1]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" 1> /dev/null 2> /dev/null
+}
+
+makeRequests >> $RESULT;
--- curl-client/prettyClient.sh
+++ curl-client/prettyClient.sh
@@ -1,55 +1,61 @@
 #!/usr/bin/env bash
 
 RESULT=`pwd`/curlResult
-echo -e `date` >> $RESULT
 
-#Creazione di tre birre POST request /api/beers
-echo "Creazione di 3 birre, di cui l'ultima è ripetuta" >> $RESULT
-echo '3 POST /api/beers response:' >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer1&type=type1&alcoholic_degree=1" | json >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" | json >> $RESULT
-curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" | json >> $RESULT #creazione fallita, birra già esistente
-echo -e '\n \n' >> $RESULT
+function makeRequests {
 
-#Ricezione di tutte le birre GET request /api/beers
-echo 'Ricezione di tutte le birre' >> $RESULT
-echo '1 GET /api/beers response:' >> $RESULT
-curl http://10.11.1.5/api/beers | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    echo -e `date` >> $RESULT
 
-#Salvataggio degli id delle birre per eseguire operazioni di ricezione, modifica e rimozione
-OUT=$(curl http://10.11.1.5/api/beers | json | grep id | awk '{print $2}' | sed 's/[",]//g')
-ar=($OUT)
+    #Creazione di tre birre POST request /api/beers
+    echo "Creazione di 3 birre, di cui l'ultima è ripetuta"
+    echo '3 POST /api/beers response:'
+    curl http://10.11.1.5/api/beers --data "name=beer1&type=type1&alcoholic_degree=1" | json
+    curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" | json
+    curl http://10.11.1.5/api/beers --data "name=beer2&type=type2&alcoholic_degree=2" | json #creazione fallita, birra già esistente
+    echo -e '\n \n'
 
-#Ricezione di una birra, GET request /api/beers/$id
-echo 'Ricezione di una birra' >> $RESULT
-echo '1 GET /api/beers/'`echo ${ar[0]}`' response' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Ricezione di tutte le birre GET request /api/beers
+    echo 'Ricezione di tutte le birre'
+    echo '1 GET /api/beers response:'
+    curl http://10.11.1.5/api/beers | json
+    echo -e '\n \n'
 
-#Modifica di una birra, PUT request /api/beers/$id
-echo 'Modifica di una birra' >> $RESULT
-echo '1 PUT /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Salvataggio degli id delle birre per eseguire operazioni di ricezione, modifica e rimozione
+    OUT=$(curl http://10.11.1.5/api/beers | json | grep id | awk '{print $2}' | sed 's/[",]//g')
+    ar=($OUT)
 
-#Modifica fallita di una birra, PUT request /api/beers/$id
-echo 'Modifica di una birra' >> $RESULT
-echo '1 PUT /api/beers/000 response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Ricezione di una birra, GET request /api/beers/$id
+    echo 'Ricezione di una birra'
+    echo '1 GET /api/beers/'`echo ${ar[0]}`' response'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` | json
+    echo -e '\n \n'
 
-#Rimozione di una birra, DELETE request /api/beers/$id
-echo 'Cancellazione di una birra' >> $RESULT
-echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Modifica di una birra, PUT request /api/beers/$id
+    echo 'Modifica di una birra'
+    echo '1 PUT /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" | json
+    echo -e '\n \n'
 
-#Rimozione fallita di una birra, DELETE request /api/beers/$id
-echo 'Cancellazione di una birra non esistente' >> $RESULT
-echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:' >> $RESULT
-curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" | json >> $RESULT
-echo -e '\n \n' >> $RESULT
+    #Modifica fallita di una birra, PUT request /api/beers/$id
+    echo 'Modifica di una birra'
+    echo '1 PUT /api/beers/000 response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=PUT" | json
+    echo -e '\n \n'
 
-#Pulizia dell'ultima birra rimasta
-curl http://10.11.1.5/api/beers/`echo ${ar[1]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" 1> /dev/null 2> /dev/null
+    #Rimozione di una birra, DELETE request /api/beers/$id
+    echo 'Cancellazione di una birra'
+    echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" | json
+    echo -e '\n \n'
+
+    #Rimozione fallita di una birra, DELETE request /api/beers/$id
+    echo 'Cancellazione di una birra non esistente'
+    echo '1 DELETE /api/beers/'`echo ${ar[0]}`' response:'
+    curl http://10.11.1.5/api/beers/`echo ${ar[0]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" | json
+    echo -e '\n \n'
+
+    #Pulizia dell'ultima birra rimasta
+    curl http://10.11.1.5/api/beers/`echo ${ar[1]}` --data "name=mod&type=mod&alcoholic_degree=100&_method=DELETE" 1> /dev/null 2> /dev/null
+}
+
+makeRequests >> $RESULT;
